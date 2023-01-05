e8b86ea53d805bddf78d164ecce070a06802f546
Daniel <daniel.agostinho@hotmail.com>
2023-01-05 16:51:39 -0300

    feat: 4.17. Utilizando Icones dentro do React

---
+++ src/components/Info/Info.tsx
@@ -0,0 +1,22 @@
+import * as I from "./Info.styles"
+import Icon from "@mdi/react";
+import {mdiInformation} from '@mdi/js'
+
+export interface InfoProps {
+    title: string
+    description: string
+}
+
+export default function InfoPros({title, description}: InfoProps) {
+    return <I.Wrapper>
+        <I.InfoInnerContent>
+            <I.InfoIcon>
+                <Icon path={mdiInformation} color={'#09f'} size={'48px'}/>
+            </I.InfoIcon>
+            <I.InfoMessages>
+                <I.InfoTitle>{title}</I.InfoTitle>
+                <p>{description}</p>
+            </I.InfoMessages>
+        </I.InfoInnerContent>
+    </I.Wrapper>
+}
