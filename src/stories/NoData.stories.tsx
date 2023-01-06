45632f91fdfedf5a9b016f70cce2ab79f82d55bb
Daniel <daniel.agostinho@hotmail.com>
2023-01-06 19:01:31 -0300

    feat: 4.26. Criando componente NoData

---
+++ src/stories/NoData.stories.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import NoData , {NoDataProps} from "../components/NoData/NoData";
+
+
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+    title: 'Example/NoData',
+    component: NoData,
+    // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+    // argTypes: {
+    //   backgroundColor: { control: 'color' },
+    // },
+} as ComponentMeta<typeof NoData>;
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template: ComponentStory<typeof NoData> = (args) => <NoData {...args} />;
+
+export const Default = Template.bind({})
+Default.args = {
+
+}
+
+export const FixedHeight = Template.bind({})
+FixedHeight.args = {
+    height: 240
+}
