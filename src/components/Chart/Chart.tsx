9c945d7bb067c306d8231c0c333a1cdcd59683f5
Daniel <daniel.agostinho@hotmail.com>
2023-01-07 22:46:10 -0300

    feat: 4.38. Finalizando o visual do Chart

--- src/components/Chart/Chart.tsx
+++ src/components/Chart/Chart.tsx
@@ -1,4 +1,7 @@
 import {Line} from "react-chartjs-2";
+import styled from "styled-components";
+import {transparentize} from "polished";
+import Heading from "../Typography/Heading/Heading";
 
 const data = {
     labels: ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun'],
@@ -25,7 +28,7 @@ const data = {
 };
 
 const options: Chart.ChartOptions = {
-    maintainAspectRatio: false,
+    maintainAspectRatio: true,
     elements: {
         line: {
             tension: 0
@@ -59,15 +62,31 @@ const options: Chart.ChartOptions = {
     },
 };
 
-export interface ChartProps {}
+export interface ChartProps {
+
+}
 
 export default function Chart ({}: ChartProps) {
-    return <div>
+    return <ChartWrapper>
+        <div style={{marginBottom: 16}}>
+            <Heading level={3}>
+                {'Média de performance nos últimos 12 meses'}
+            </Heading>
+
+        </div>
         <Line
             type="line"
-            height={250}
+            height={139}
+            width={600}
             data={data}
             options={options}
         />
-    </div>
+    </ChartWrapper>
 }
+
+const ChartWrapper = styled.div`
+  text-align: center;
+  border: 1px solid ${transparentize(0.9, '#274060')};
+  padding: 20px;
+  width: 640px;
+`
