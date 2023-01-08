20519e4f4b83d5a1bfe3053dba1acba76e0ad88b
Daniel <daniel.agostinho@hotmail.com>
2023-01-08 20:54:26 -0300

    feat: 4.48. Desafio - SessionController

---
+++ src/stories/SessionController.stories.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import SessionController , {SessionControllerProps} from "../components/SessionController/SessionController";
+
+
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+    title: 'Example/SessionController',
+    component: SessionController,
+    // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+    // argTypes: {
+    //   backgroundColor: { control: 'color' },
+    // },
+} as ComponentMeta<typeof SessionController>;
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template: ComponentStory<typeof SessionController> = (args) => <SessionController {...args} />;
+
+export const Default = Template.bind({})
+Default.args = {
+    name: 'Fulano da Silva',
+    description: 'editor ha muito tempo'
+}
+
