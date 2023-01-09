b8354645f3cca7596709f8bc2eeab444e1352b40
Daniel <daniel.agostinho@hotmail.com>
2023-01-08 21:01:51 -0300

    feat: 4.49. Criando o contador de palavras

---
+++ src/stories/WordPriceCounter.stories.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import WordPriceCounter , {WordPriceCounterProps} from "../components/WordPriceCounter/WordPriceCounter";
+
+
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+    title: 'Example/WordPriceCounter',
+    component: WordPriceCounter,
+    // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+    // argTypes: {
+    //   backgroundColor: { control: 'color' },
+    // },
+} as ComponentMeta<typeof WordPriceCounter>;
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template: ComponentStory<typeof WordPriceCounter> = (args) => <WordPriceCounter {...args} />;
+
+export const Default = Template.bind({})
+Default.args = {
+    pricePerWord: 0.25,
+    wordsCount: 20
+}
