08607d3a4fc2d978c82e26e42452252bbde28bba
Daniel <daniel.agostinho@hotmail.com>
2023-01-09 13:14:32 -0300

    feat: 4.54. Editor de texto em Markdown

---
+++ src/stories/MarkdownEditor.stories.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import MarkdownEditor , {MarkdownEditorProps} from "../components/MardownEditor";
+
+
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+    title: 'Example/MarkdownEditor',
+    component: MarkdownEditor
+} as ComponentMeta<typeof MarkdownEditor>;
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template: ComponentStory<typeof MarkdownEditor> = (args: MarkdownEditorProps) => <MarkdownEditor {...args} />;
+
+export const Default = Template.bind({})
+Default.args = {
+
+}
+
