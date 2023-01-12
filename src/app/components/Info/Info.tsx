697b68afac7e71ae90eadc96bfb14b34758c0282
Daniel <daniel.agostinho@hotmail.com>
2023-01-12 16:56:54 -0300

    feat: 6.18. Transformando um método em uma macrotask (event loop)

--- src/app/components/Info/Info.tsx
+++ src/app/components/Info/Info.tsx
@@ -8,15 +8,17 @@ export interface InfoProps {
 }
 
 export default function InfoPros({title, description}: InfoProps) {
-    return <I.Wrapper>
-        <I.InfoInnerContent>
-            <I.InfoIcon>
-                <Icon path={mdiInformation} color={'#09f'} size={'48px'}/>
-            </I.InfoIcon>
-            <I.InfoMessages>
-                <I.InfoTitle>{title}</I.InfoTitle>
-                <p>{description}</p>
-            </I.InfoMessages>
-        </I.InfoInnerContent>
-    </I.Wrapper>
+    setTimeout(() => {
+        return <I.Wrapper>
+            <I.InfoInnerContent>
+                <I.InfoIcon>
+                    <Icon path={mdiInformation} color={'#09f'} size={'48px'}/>
+                </I.InfoIcon>
+                <I.InfoMessages>
+                    <I.InfoTitle>{title}</I.InfoTitle>
+                    <p>{description}</p>
+                </I.InfoMessages>
+            </I.InfoInnerContent>
+        </I.Wrapper>
+    }, 0)
 }
--- src/core/utils/confirm.tsx
+++ src/core/utils/confirm.tsx
@@ -9,22 +9,24 @@ interface ConfirmProps {
 }
 
 export default function confirm({title, onConfirm, onCancel}: ConfirmProps){
-    confirmAlert({
-        overlayClassName: 'confirm-overlay',
-        customUI: ({onClose}) => {
-            return (
-                <Confirm
-                    title={title}
-                    onConfirm={() => {
-                        if(onConfirm) onConfirm()
-                        onClose()
-                    }}
-                    onCancel={() => {
-                        if(onCancel) onCancel()
-                        onClose()
-                    }}
-                />
-            )
-        }
-    })
+    setTimeout(() => {
+        confirmAlert({
+            overlayClassName: 'confirm-overlay',
+            customUI: ({onClose}) => {
+                return (
+                    <Confirm
+                        title={title}
+                        onConfirm={() => {
+                            if(onConfirm) onConfirm()
+                            onClose()
+                        }}
+                        onCancel={() => {
+                            if(onCancel) onCancel()
+                            onClose()
+                        }}
+                    />
+                )
+            }
+        })
+    }, 0)
 }
